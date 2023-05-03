b6e9096fce2d8504918beedff38317390205e374
Michael Peter <michaelhpet@gmail.com>
2023-05-03 20:22:49 +0100

    feat: add /users endpoint

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ import express from "express"
 
 const app = express()
 
+// app.get("/")
+// app.post("/user")
+// app.put()
+// app.delete()
+
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" })
+  console.log("ohhhh interesting, a request just came in")
+  return response.json({ name: "John Doe" })
+})
+
+app.get("/users", (request, response) => {
+  console.log("yay... querying the users from the database")
+  return response.json({ users: ["Bob", "Alice", "John", "Doe", "Joe"] })
 })
 
 app.listen(5000, () => {
