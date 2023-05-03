d5008ad5471a337edc598e52bda21ca549ddc4b5
Michael Peter <michaelhpet@gmail.com>
2023-05-03 21:04:57 +0100

    feat: mock database file

---
+++ database.js
@@ -0,0 +1,117 @@
+// A mock database to simulate data storage and retrieval
+
+export const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: false,
+  },
+  {
+    id: 2,
+    name: "Alice James",
+    phone: "2349750341698",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: true,
+  },
+  {
+    id: 3,
+    name: "James Bond",
+    phone: "2348756341098",
+    address: "13A, FLowers Estate, Lekki",
+    subscribed: false,
+  },
+  {
+    id: 4,
+    name: "Deborah Adams",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: false,
+  },
+  {
+    id: 5,
+    name: "Alice Bob",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: true,
+  },
+  {
+    id: 6,
+    name: "Naomi Bond",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: false,
+  },
+  {
+    id: 7,
+    name: "Sarah Peters",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: false,
+  },
+  {
+    id: 8,
+    name: "Babatunde Ajayi",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: false,
+  },
+  {
+    id: 9,
+    name: "Chukwuemeka Mba",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: false,
+  },
+  {
+    id: 10,
+    name: "Smith Kenneth",
+    phone: "2348756341098",
+    address: "13A, Johnsons Str., Maryland",
+    subscribed: true,
+  },
+]
+
+export const todos = [
+  {
+    id: 1,
+    authorId: 1,
+    title: "Recipe list",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, ipsum. Inventore ipsam soluta tempore ducimus, voluptatibus officia ea maxime ratione fugiat quis dolore! Recusandae, laboriosam. Aspernatur vel eligendi ab modi?",
+    createdAt: "Jan 15, 2025",
+  },
+  {
+    id: 2,
+    authorId: 5,
+    title: "Recipe list",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, ipsum. Inventore ipsam soluta tempore ducimus, voluptatibus officia ea maxime ratione fugiat quis dolore! Recusandae, laboriosam. Aspernatur vel eligendi ab modi?",
+    createdAt: "Jan 15, 2025",
+  },
+  {
+    id: 3,
+    authorId: 2,
+    title: "Recipe list",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, ipsum. Inventore ipsam soluta tempore ducimus, voluptatibus officia ea maxime ratione fugiat quis dolore! Recusandae, laboriosam. Aspernatur vel eligendi ab modi?",
+    createdAt: "Jan 15, 2025",
+  },
+  {
+    id: 4,
+    authorId: 1,
+    title: "Recipe list",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, ipsum. Inventore ipsam soluta tempore ducimus, voluptatibus officia ea maxime ratione fugiat quis dolore! Recusandae, laboriosam. Aspernatur vel eligendi ab modi?",
+    createdAt: "Jan 15, 2025",
+  },
+  {
+    id: 5,
+    authorId: 1,
+    title: "Recipe list",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, ipsum. Inventore ipsam soluta tempore ducimus, voluptatibus officia ea maxime ratione fugiat quis dolore! Recusandae, laboriosam. Aspernatur vel eligendi ab modi?",
+    createdAt: "Jan 15, 2025",
+  },
+]
--- index.js
+++ index.js
@@ -2,21 +2,6 @@ import express from "express"
 
 const app = express()
 
-// app.get("/")
-// app.post("/user")
-// app.put()
-// app.delete()
-
-app.get("/", (request, response) => {
-  console.log("ohhhh interesting, a request just came in")
-  return response.json({ name: "John Doe" })
-})
-
-app.get("/users", (request, response) => {
-  console.log("yay... querying the users from the database")
-  return response.json({ users: ["Bob", "Alice", "John", "Doe", "Joe"] })
-})
-
 app.listen(5000, () => {
   console.log("Now running on port 5000")
 })
