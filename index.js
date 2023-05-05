69e794398de5168e00847015c4765e95c6fa9c2a
Michael Peter <michaelhpet@gmail.com>
2023-05-05 21:30:26 +0100

    feat: put and delete requests for /users

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from "cors"
 import path from "path"
 import { usersRouter } from "./routes/users.js"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (request, response) => {
--- routes/users.js
+++ routes/users.js
@@ -57,6 +57,70 @@ router.post("/", (request, response) => {
   })
 })
 
-const numbers = [1, 5, 4, 9, 6, 0, 1, 4, 6, 7, 9] // 11 elements
+// PUT (update) existing user
+router.put("/:id", (request, response) => {
+  const id = request.params.id
+  const body = request.body
+
+  if (body.id) {
+    return response.status(403).json({
+      error: {
+        code: 403,
+        message: "You cannot update your id"
+      }
+    })
+  }
+
+  const acceptedProperties = ["name", "phone", "address"]
+
+  for (const key in body) {
+    if (!acceptedProperties.includes(key)) {
+      return response.status(403).json({
+        error: {
+          code: 403,
+          message: "Unsupported property. Please update only name, phone, and address"
+        }
+      })
+    }
+  }
+
+  const user = users.find((element) => element.id === Number(id))
+  const updatedUser = {...user, ...body}
+  users[users.indexOf(user)] = updatedUser
+
+  return response.status(200).json({
+    data: {
+      code: 200,
+      message: "User updated successfully",
+      user: updatedUser
+    }
+  })
+
+})
+
+// DELETE a user
+router.delete("/:id", (request, response) => {
+  const id = request.params.id
+  const user = users.find(element => element.id === Number(id))
+
+  if (!user) {
+    return response.status(404).json({
+      error: {
+        code: 404,
+        message: "User not found"
+      }
+    })
+  }
+
+  users.splice(users.indexOf(user), 1)
+
+  return response.status(200).json({
+    data: {
+      success: true,
+      message: "User deleted"
+    }
+  })
+
+})
 
 export const usersRouter = router
